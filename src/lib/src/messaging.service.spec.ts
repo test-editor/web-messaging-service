15703ff7f8a55a6bcf49705f7f12c88b485223d0
Franz Becker <franz.becker@signal-iduna.de>
2017-08-02 17:45:52 +0200

    implement MessagingService

--- src/lib/src/messaging.service.spec.ts
+++ src/lib/src/messaging.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { MessagingService } from './messaging.service';
 
+type MessageCallback = (payload: any) => void;
+
 describe('MessagingService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -11,6 +13,31 @@ describe('MessagingService', () => {
 
   it('should create service', inject([MessagingService], (service: MessagingService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should deliver a message if subscribed', inject([MessagingService], (service: MessagingService) => {
+    // given
+    let callback = jasmine.createSpy('callback');
+    service.subscribe('theTopic', callback);
+
+    // when
+    service.publish('theTopic', 'anyPayload');
+
+    // then
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith('anyPayload');
+  }));
+
+  it('should not deliver messages on unrelated topic', inject([MessagingService], (service: MessagingService) => {
+    // given
+    let callback = jasmine.createSpy('callback');
+    service.subscribe('theTopic', callback);
+
+    // when
+    service.publish('unrelatedTopic', 'anyPayload');
+
+    // then
+    expect(callback).not.toHaveBeenCalled();
   }));
 
 });
